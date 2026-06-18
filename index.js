532ed1dde2fb22c0bac65889213d33010b68ad6c
geptag <geppttag@gmail.com>
2026-06-17 21:02:21 -0300

    commitdenovo222

---
+++ index.js
@@ -0,0 +1,7 @@
+let pokemon = ["rosa", "vermelho", "quadrado"]
+
+pokemon.forEach((nome) => {console.log(nome),console.log("-----------")});
+
+pokerocks.innerHTML = `<h1>${todosPokemon}</h1>`
+
+todosPokemon.forEach((nome) => {<h1>nome</h1>});
